5ec6ff39addf112c923b44b5a0a86864dbefa342
aditikeshri5 <aditi.keshri180706@gmail.com>
2026-08-20 10:23:43 +0530

    Fix Flask import

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-# Add backend folder to Python path
-backend_path = os.path.join(
-    os.path.dirname(__file__),
-    "..",
-    "backend"
+sys.path.insert(
+    0,
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "backend"
+    )
 )
-
-sys.path.insert(0, backend_path)
 
 from app import app
